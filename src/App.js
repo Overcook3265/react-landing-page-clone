4005169607034b508a1c9cbd31147a31110e9042
Overcook3265 <151878132+Overcook3265@users.noreply.github.com>
2024-01-23 15:30:18 +0100

    Setup basic react app structure

---
+++ src/App.js
@@ -0,0 +1,40 @@
+import styles from './App.module.scss';
+
+export default function App() {
+  return (
+    <div>
+      <header>
+        <nav>
+          <ul>
+            <li>
+              <a href="/">About</a>
+            </li>
+            <li>
+              <a href="/">Blog</a>
+            </li>
+            <li>
+              <a href="/">Reviews</a>
+            </li>
+            <li>
+              <a href="/">Log In</a>
+            </li>
+            <li>
+              <button>Get started</button>
+            </li>
+          </ul>
+        </nav>
+      </header>
+      <main>
+        <div>Quit drinking or else!</div>
+      </main>
+      <section />
+      <section />
+      <section />
+      <section />
+      <section />
+      <section />
+      <section />
+      <footer>I'm a footer</footer>
+    </div>
+  );
+}
